b6c25b427fd97b5d3822498cd4327145fd27c6fd
lucax13 <lucas13lima2006@gmail.com>
2025-03-26 11:56:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,31 @@ app.get('/', (req, res) => {
     res.send('API utilizando Node.js, Express e MySQL');
 });
 
-app.listen(porta, () => {
+//Exibindo toso os alunos
+app.get('/alunos', (req, res) => {
+    res.send('Exibindo todos os alunos');
+});
+
+//exibndo um aluno
+app.get('/alunos/1', (req, res) => {
+    res.send('exibindo um aluno ');
+});
+
+//inserindo aluno
+app.post('/alunos', (req, res) => {
+    res.send('exibindo dados do aluno');
+});
+
+//atualizando aluno
+app.patch('alunos/1', (req, res) => {
+    res.send('atualizar dados aluno');
+});
+
+//excluir aluno
+app.delete ('alunos/1', (req, res) => {
+    res.send('aluno excluindo com sucesso!');
+});
+
+app.listen(porta, (req, res) => {
     console.log('servidor rodando na porta ${porta}');
 });
